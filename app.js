9b0ab927480f31f027eb97433369539d12566a52
kushal <kushalsinghnegi72@gmail.com>
2024-07-26 21:12:29 +0530

    second change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
@@ -16,10 +16,16 @@ io.on("connect",(socket)=>{
     socket.on("send-location",(data)=>{
         io.emit("recieve-location", {id: socket.id, ...data});
     })
+
+    socket.on("disconnet",()=>{
+        io.emit("user-disconnect",socket.id);
+    })
 })
 
 app.get("/",function(req,res){
     res.send({ message: "server connected" });
 })
 
-server.listen(PORT);
+server.listen(3000,()=>{
+    console.log("server connected");
+});
